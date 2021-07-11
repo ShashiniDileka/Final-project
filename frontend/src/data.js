931ebd24f87dd40209ae7e4979a735c86ba59bfc
ShashiniDileka <92947@fhss.sjp.ac.lk>
2021-07-11 10:29:56 +0530

    frontend development

---
+++ frontend/src/data.js
@@ -0,0 +1,105 @@
+export default  {
+    products:[
+     {
+        name:'Slim Shirt',
+        category:'Shirts',
+        images:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',  
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slims Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim tShirt',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      {
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+      },
+      
+
+]
+}
