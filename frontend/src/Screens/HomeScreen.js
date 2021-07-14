71435bf3cb626e346923fec6a0751cf5b57c4673
ShashiniDileka <92947@fhss.sjp.ac.lk>
2021-07-14 12:55:25 +0530

    frontend development

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -8,7 +8,13 @@ function HomeScreen (props){
        data.products.map(product=>
         <li>
           <div className="product">
-          <Link to ={'/product/'+product._id}>{product.name}</Link>
+          <Link to ={'/product/'+product._id}>
+            <img
+               className="product-image"
+               src={product.image}
+               alt="product"
+               />
+          </Link>
              
              <div className="product-name">
                 <Link to ={'/product/'+product._id}>{product.name}</Link>
@@ -16,6 +22,7 @@ function HomeScreen (props){
           <div className="product-brand">{product.brand}</div>
           <div className="product-price">{product.price}</div>
           <div className="product-rating">{product.rating} Stars({product.numRewiew})</div>
+             
          </div>
        </li>)
      }                               
