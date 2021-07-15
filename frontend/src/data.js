1fb4557a96177b7b4ce9d298be3e842354750fb3
ShashiniDileka <92947@fhss.sjp.ac.lk>
2021-07-16 00:44:28 +0530

    frontend development

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,125 +1,125 @@
 export default  {
-    products:
-    [
-     {  _id:'1',
-        name:'Carrot',
-        category:'Slim Shirt',
-        image:'/images/d1.png',
-        price:200,
-        brand:'1KG',  
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'2',
-        name:'Slim Shirt',
-        category:'Shirts',
-        image:'/images/d2.jpg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'3',
-        name:'Fit Shirt',
-        category:'Shirts',
-        image:'/images/d18.jpg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'4',
-        name:'Slims Shirt',
-        category:'Shirts',
-        image:'/images/d9.jpg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
+  products:
+  [
+   {  _id:'1',
+      name:'Carrot',
+      category:'Slim Shirt',
+      image:'/images/d1.png',
+      price:200,
+      brand:'1KG',  
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'2',
+      name:'Slim Shirt',
+      category:'Shirts',
+      image:'/images/d2.jpg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'3',
+      name:'Fit Shirt',
+      category:'Shirts',
+      image:'/images/d18.jpg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'4',
+      name:'Slims Shirt',
+      category:'Shirts',
+      image:'/images/d9.jpg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
 
-      {  _id:'5',
-        name:'Shhhnnnnh',
-        category:'Shirts',
-        image:'/images/d11.png',
-        price:60,
-        brand:'Nike',  
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'6',
-        name:'Slim Shirt',
-        category:'Shirts',
-        image:'/images/d13.jpeg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'7',
-        name:'Fit Shirt',
-        category:'Shirts',
-        image:'/images/d19.jpg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'8',
-        name:'Slims Shirt',
-        category:'Shirts',
-        image:'/images/d14.jpeg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
+    {  _id:'5',
+      name:'Shhhnnnnh',
+      category:'Shirts',
+      image:'/images/d11.png',
+      price:60,
+      brand:'Nike',  
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'6',
+      name:'Slim Shirt',
+      category:'Shirts',
+      image:'/images/d13.jpeg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'7',
+      name:'Fit Shirt',
+      category:'Shirts',
+      image:'/images/d19.jpg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'8',
+      name:'Slims Shirt',
+      category:'Shirts',
+      image:'/images/d14.jpeg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
 
-      {  _id:'9',
-        name:'Shhhnnnnh',
-        category:'Shirts',
-        image:'/images/d16.jpeg',
-        price:60,
-        brand:'Nike',  
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'10',
-        name:'Slim Shirt',
-        category:'Shirts',
-        image:'/images/d22.jpg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'11',
-        name:'Fit Shirt',
-        category:'Shirts',
-        image:'/images/d14.jpeg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
-      {
-        _id:'12',
-        name:'Slims Shirt',
-        category:'Shirts',
-        image:'/images/d20.jpg',
-        price:60,
-        brand:'Nike',
-        rating:4.5,
-        numReviews:10
-      },
-      
-    ]
+    {  _id:'9',
+      name:'Shhhnnnnh',
+      category:'Shirts',
+      image:'/images/d16.jpeg',
+      price:60,
+      brand:'Nike',  
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'10',
+      name:'Slim Shirt',
+      category:'Shirts',
+      image:'/images/d22.jpg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'11',
+      name:'Fit Shirt',
+      category:'Shirts',
+      image:'/images/d14.jpeg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
+    {
+      _id:'12',
+      name:'Slims Shirt',
+      category:'Shirts',
+      image:'/images/d20.jpg',
+      price:60,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10
+    },
+    
+  ]
 }
