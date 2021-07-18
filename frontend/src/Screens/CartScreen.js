339e81496e11424cba3f6bb98eb66a3d69dcdc9d
ShashiniDileka <92947@fhss.sjp.ac.lk>
2021-07-18 19:37:29 +0530

    frontend development

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react';
-import { addToCart } from '../actions/cartActions';
-import{ useDispatch, useSelector} from 'react-redux'
+import { addToCart,removeFromCart} from '../actions/cartActions';
+import{ useDispatch,useSelector} from 'react-redux'
+import { Link } from 'react-router-dom';
 
 function CartScreen (props){
     const cart= useSelector(state => state.cart);
@@ -9,6 +10,10 @@ function CartScreen (props){
     const productId= props.match.params.id;
     const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
     const dispatch = useDispatch();
+    const removeFromCartHandler=(productId)=>{
+      dispatch(removeFromCart(productId));
+    }
+    
 
     useEffect(() => {
         if (productId) {
@@ -16,6 +21,11 @@ function CartScreen (props){
           //
         }
       }, []);
+
+    const checkoutHandler = () => {
+        props.history.push("/signin?redirect=shipping");
+    }
+    
 
     return <div className="cart"> 
       <div className="cart-list">
@@ -35,25 +45,33 @@ function CartScreen (props){
                 </div>
                 :
                 cartItems.map(item =>
-                  <div>
-                     <img src={item.image} alt="product" />   
+                 <li>
+                    <div className="cart-image">
+                         <img src={item.image} alt="product" /> 
+                    </div>  
                      <div className="cart-name">
-                     <div>
+                       <div>
+                        <Link to={"/product/" + item.product}> 
                         {item.name}
-                     </div>
-                     <div>
+                        </Link>
+                        
+                       </div>
+                       <div>
                        Qty:
-                       <select>
+                       <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, e.target.value))}>
                          <option valu="1">1</option>
                          <option valu="2">2</option>
                          <option valu="3">3</option>
                        </select>
+                       <button type="button" className="button" onClick={() => removeFromCartHandler(item.product)} >
+                         Delete
+                       </button>
                      </div>
                     </div>
-                    <div>
+                    <div className="cart-price">
                       {item.price}
                   </div>
-                </div>
+                </li>
               )
 
             }
@@ -64,9 +82,9 @@ function CartScreen (props){
            Subtotal ( {cartItems.reduce((a, c) => a + c.qty, 0)} items)
            
            :
-           $ {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+             $ {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
            </h3>
-           <button className="button primary" disabled={cartItems.length ===0}>
+           <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0}>
             Proceed to Checkout
            </button>
          </div>
--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,6 @@
-import Axios from "axios"
-import { CART_ADD_ITEM} from "../constants/cartConstants";
+import Axios from "axios";
+import Cookie from "js-cookie";
+import { CART_ADD_ITEM,CART_REMOVE_ITEM} from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch, getState) => {
 
@@ -7,14 +8,17 @@ const addToCart = (productId, qty) => async (dispatch, getState) => {
          const { data } = await Axios.get("/api/products/" + productId);
          dispatch({
             type: CART_ADD_ITEM, payload: {
-            product: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock, 
-            qty
-       
-           }});
+               product: data._id,
+               name: data.name,
+               image: data.image,
+               price: data.price,
+               countInStock: data.countInStock, 
+               qty
+            }
+         });
+
+         const { cart: { cartItems } } = getState();
+         Cookie.set("cartItems", JSON.stringify(cartItems));
     
 
     } catch (error) {
@@ -23,4 +27,11 @@ const addToCart = (productId, qty) => async (dispatch, getState) => {
     }
 }
 
-export { addToCart }
+
+const removeFromCart = (productId)=>(dispatch, getState)=>{
+   dispatch({type:CART_REMOVE_ITEM,payload:productId});
+
+   const { cart: { cartItems } } = getState();
+   Cookie.set("cartItems", JSON.stringify(cartItems));
+}
+export { addToCart ,removeFromCart}
